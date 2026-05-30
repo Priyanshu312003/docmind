e9ee1b6c5d066f880f6e5ae4994ff6b86dde0e45
Priyanshu Maurya <priyanshumaurya.2003@gmail.com>
2026-05-31 02:03:20 +0530

    feat: implement chunk_text function

--- ingest.py
+++ ingest.py
@@ -7,3 +7,14 @@ def load_pdf(path: str) -> str:
         text = page.get_text()
         full_text += text
     return full_text
+
+def chunk_text(text: str, chunk_size: int =500, overlap: int = 50) ->list:
+    chunks = []
+    words = text.split()
+    i = 0
+    while i < len(words):
+        chunk = words[i:i+chunk_size]
+        chunk = " ".join(chunk)
+        chunks.append(chunk)
+        i += chunk_size - overlap
+    return chunks
